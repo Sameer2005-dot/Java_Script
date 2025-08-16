a7a4dc62ccb9421c22ca43651004c27e26e86277
Sameer2005-dot <jena80225@gmail.com>
2025-08-16 23:23:18 +0530

    Added basics of js

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,19 @@
+
+const accountID = 335
+let accountEmail = "jena12@gmail.com"
+var accountPassword = "1234"
+accountCity = "khordha"
+let accountState;
+
+// accountID = 336 // It is not allowed.
+
+accountEmail = "sameer12@gmail.com"
+accountPassword = "2005"
+accountCity = "Khordha"
+
+
+console.log("MyID = " + accountID);
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState]);
+
+
