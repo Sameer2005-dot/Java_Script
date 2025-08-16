b5e439aec984f1e3bf0c397eeece294cae73d6f7
Sameer2005-dot <jena80225@gmail.com>
2025-08-17 00:40:52 +0530

    Coversion some datatypes

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -26,7 +26,40 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 
+/* ...................Operations...............*/
+
+let value = 5
+let negValue = - value
+// console.log(negValue);
+
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 4);
+// console.log(2 / 2);
+// console.log(5 % 2);
+
+
+str1 = "Hello"
+str2 = " Sameer"
+
+str3 = str1 + str2;
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+
+
+let gameCounter = 100;
+gameCounter ++;
+console.log(gameCounter);
