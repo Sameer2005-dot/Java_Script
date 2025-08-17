c207beef656e82b2aec4b5958a153567eccc78bf
Sameer2005-dot <jena80225@gmail.com>
2025-08-18 02:08:25 +0530

    Added Maths method

--- 01_Basics/06_nums_and_maths.js
+++ 01_Basics/06_nums_and_maths.js
@@ -1,15 +1,39 @@
 const score = 400
-console.log(score);
+// console.log(score);
 
 const balance = new Number(100)
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString());
-console.log(balance.toFixed(3));
+// console.log(balance.toString());
+// console.log(balance.toFixed(3));
 
 const newBalance = 123.45455
-console.log(newBalance.toPrecision(5));
+// console.log(newBalance.toPrecision(5));
 
 
 const hundreds = 1000000
-console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// ......................Maths......................//
+
+console.log(Math);
+
+// console.log(Math.abs(-5));
+// console.log(Math.round(5.6));
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(5.6));
+// console.log(Math.max(2, 4, 6, 9));
+// console.log(Math.min(2, 4, 6, 9));
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
