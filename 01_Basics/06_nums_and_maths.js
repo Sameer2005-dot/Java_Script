d4c257cefa48415174702129389b268779049550
Sameer2005-dot <jena80225@gmail.com>
2025-08-18 01:48:02 +0530

    Added num methods

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,15 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toFixed(3));
+
+const newBalance = 123.45455
+console.log(newBalance.toPrecision(5));
+
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
