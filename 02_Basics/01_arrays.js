3ea0feec0e49989d87f8c3e401c6ec1b3491593a
Sameer2005-dot <jena80225@gmail.com>
2025-08-19 23:58:42 +0530

    added some basics of arrays

--- 02_Basics/01_arrays.js
+++ 02_Basics/01_arrays.js
@@ -2,4 +2,12 @@
 
 const myArr = [1, 2, 3, 4, 5]
 
+const myHeros = ["Sameer", "Arpit", "Biswajti"]
+
+const myArr1 = new Array(6, 7, 8, 9, 10)
+
+
 console.log(myArr[0]);
+console.log(myHeros[1]);
+console.log(myArr1);
+
