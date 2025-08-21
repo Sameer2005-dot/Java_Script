cb79fa3c2e086f3d2635c649938000b787de2287
Sameer2005-dot <jena80225@gmail.com>
2025-08-22 02:06:42 +0530

    added function with objecta and array

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -35,4 +35,36 @@ function loginUserMessage(userName = "Sam") {  //here "Sam" is use for default v
 }
 
 // console.log(loginUserMessage("sameer"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage("SAMEER"));
+
+
+function calculateCardPrice(val1, val2, ...num1) { //here val1 and val2 give some separate num
+    return num1
+}
+// console.log(calculateCardPrice(200, 400, 500, 1000));
+
+
+// by using object
+const user = {
+    userName: "Sameer",
+    courseName: "JS"
+}
+
+function handleObject(anyObject) {
+    console.log(`Username is ${anyObject.userName} and the course name is ${anyObject.courseName}`);
+}
+
+handleObject(user)
+
+// By using array
+
+const myNewArray = [100, 500, 1000, 1500]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));        //OR
+
+console.log(returnSecondValue([100, 500, 1000, 1500]));
+
