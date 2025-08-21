a3f21803a0ac42717149badd086ea2f5ff10fde1
Sameer2005-dot <jena80225@gmail.com>
2025-08-22 01:04:51 +0530

    Update objects and JSON API talk

---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,29 @@
+
+const course = {
+    courseName: "JavaScript",
+    price: "999",
+    courseInstructor: "Sameer"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: Instructor} = course
+
+// console.log(courseInstructor);
+console.log(Instructor);
+
+
+//   JSON API   //
+
+// {
+//     name: "sameer",
+//     courseName: "JS",
+//     price: "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
